d8c1c6cab9c729a0bd167cd486689b8db1563ae7
Fikri Ahmad <rukimevick@gmail.com>
2025-09-27 23:21:56 +0700

    Update TestUI_Gameplay.cs

--- Assets/Scripts/UI/[Test] UI Gameplay/TestUI_Gameplay.cs
+++ Assets/Scripts/UI/[Test] UI Gameplay/TestUI_Gameplay.cs
@@ -1,8 +1,16 @@
+#region Summary
+// This script is used for testing the gameplay mechanics functionality such as saving and loading the game through UI interaction.
+#endregion
+
 using UnityEngine;
 
 public class TestUI_Gameplay : MonoBehaviour
 {
-    public void OnClick_SaveGane()
+    [Header("Scene Names")]
+    public string sceneToLoadName;
+    public string sceneToUnloadName;
+    
+    public void OnClick_SaveGame()
     {
         System_DataPersistenceManager.instance.SaveGame();
         Debug.Log("Save Game Clicked");
@@ -10,7 +18,9 @@ public class TestUI_Gameplay : MonoBehaviour
 
     public void OnClick_LoadGame()
     {
-        System_DataPersistenceManager.instance.LoadGame();
+        System_UniversalLoadingScreen.instance.sceneToLoadName = this.sceneToLoadName;
+        System_UniversalLoadingScreen.instance.sceneToUnloadName = this.sceneToUnloadName;
+        System_UniversalLoadingScreen.instance.SetIsLoadGame(true);
         Debug.Log("Load Game Clicked");
     }
 }
