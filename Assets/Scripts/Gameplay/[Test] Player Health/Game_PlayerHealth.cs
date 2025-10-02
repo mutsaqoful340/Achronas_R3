0e5764fe5b3ac537894a1f1237f021c2375c1c80
Fikri Ahmad <rukimevick@gmail.com>
2025-10-02 22:51:13 +0700

    Moving PlayerHealth.cs

---
+++ Assets/Scripts/Gameplay/[Test] Player Health/Game_PlayerHealth.cs
@@ -0,0 +1,36 @@
+using TMPro;
+using UnityEngine;
+
+public class Game_PlayerHealth : MonoBehaviour, System_IDataPersistence
+{
+
+    [Header("Player Health")]
+    [SerializeField] private int currentPlayerHealth = 100;
+    public TextMeshProUGUI healthText;
+
+    public void LoadData(System_GameData data)
+    {
+        this.currentPlayerHealth = data.currentPlayerHealth;
+        UpdateHealthDisplay();
+    }
+
+    public void SaveData(ref System_GameData data)
+    {
+        data.currentPlayerHealth = this.currentPlayerHealth;
+    }
+
+    void Start()
+    {
+        healthText.text = "Health: " + this.currentPlayerHealth;
+    }
+
+    public void TakeDamage()
+    {
+        this.currentPlayerHealth -= 10;
+        UpdateHealthDisplay();
+    }
+    private void UpdateHealthDisplay()
+    {
+        healthText.text = "Health: " + this.currentPlayerHealth;
+    }
+}
